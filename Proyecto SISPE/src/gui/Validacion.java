0e106caf6e66b3a2708f75b58229450b03766a31
camilo20121390 <milo15_24@hotmail.com>
2014-10-08 17:41:07 -0500

    nuevos paquetes ingresados

--- Proyecto SISPE/src/gui/Validacion.java
+++ Proyecto SISPE/src/gui/Validacion.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import persistence.Conexion;
+
 @WebServlet("/Validacion")
 public class Validacion extends HttpServlet {
 	
@@ -26,6 +28,10 @@ public class Validacion extends HttpServlet {
 		String nombreusuario=request.getParameter("nombreusuario");
 		String contrasenha=request.getParameter("contrasenha");
 
+		Conexion conexion=new Conexion();
+		
+		
+		
 		PrintWriter escritor=response.getWriter();
 		escritor.write(nombreusuario);
 		escritor.write(contrasenha);
