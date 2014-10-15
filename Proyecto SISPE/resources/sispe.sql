1330efd9f42e1e1495d5e0aa98b16c3f14cd014d
Rafael Pinzón <rafa3040@hotmail.com>
2014-10-15 10:39:27 -0500

    sispe.sql corregido

--- Proyecto SISPE/resources/sispe.sql
+++ Proyecto SISPE/resources/sispe.sql
@@ -3,7 +3,7 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE SCHEMA IF NOT EXISTS `sispe` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `SISPE` ;
+USE `sispe` ;
 
 -- -----------------------------------------------------
 -- Table `sispe`.`persona`
