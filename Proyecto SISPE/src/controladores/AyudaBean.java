10cf4493feea9cf3975b75c242c73e9a1c91d30c
Rafael Pinzón <rafa3040@hotmail.com>
2014-11-02 17:18:51 -0500

    Módulo de ayuda agregado, en donde se puede descargar el formato de excel para realizar la carga de hojas de vida.

---
+++ Proyecto SISPE/src/controladores/AyudaBean.java
@@ -0,0 +1,29 @@
+package controladores;
+
+import java.io.InputStream;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+
+@ManagedBean
+@SessionScoped
+public class AyudaBean {
+	
+	private StreamedContent formatoCarga;
+	
+	public AyudaBean() {
+        InputStream flujoEntrada = ((ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream("/resources/archivos/formato carga.xls");
+        formatoCarga = new DefaultStreamedContent(flujoEntrada, "application/vnd.ms-excel", "formato carga.xls");
+	}
+
+	public StreamedContent getFormatoCarga() {
+		return formatoCarga;
+	}
+
+}
