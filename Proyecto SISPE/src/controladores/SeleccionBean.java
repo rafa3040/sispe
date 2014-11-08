e5a65a4044dc749a33129cf8c4f2429431a5d11f
Rafael Pinzón <rafa3040@hotmail.com>
2014-11-08 00:43:59 -0500

    El formulario de selección de hojas de vida permite desactivar criterios de selección

--- Proyecto SISPE/src/controladores/SeleccionBean.java
+++ Proyecto SISPE/src/controladores/SeleccionBean.java
@@ -2,6 +2,7 @@ package controladores;
 
 import java.util.ArrayList;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -20,13 +21,18 @@ public class SeleccionBean {
 	private String profesion;
 	private String especializacion;
 	private int mesesExperiencia;
+	
+	private boolean activarEdad, activarProfesion, activarEspecializacion, activarMesesExperiencia;
+	
 	private ArrayList<HojaVida> hojasVida;
 	
 	public SeleccionBean() {
 		HttpSession sesion=(HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(false);
 		gestionModelo=(GestionModelo) sesion.getAttribute("gestionModelo");
-		edadMinima=18;
-		edadMaxima=28;
+		establecerRangoEdades();
+		profesion="";
+		especializacion="";
+		mesesExperiencia=0;
 	}
 
 	public int getEdadMinima() {
@@ -69,6 +75,50 @@ public class SeleccionBean {
 		this.mesesExperiencia = mesesExperiencia;
 	}
 
+	public boolean isActivarEdad() {
+		return activarEdad;
+	}
+
+	public void setActivarEdad(boolean activarEdad) {
+		if (!activarEdad) {
+			establecerRangoEdades();
+		}
+		this.activarEdad = activarEdad;
+	}
+
+	public boolean isActivarProfesion() {
+		return activarProfesion;
+	}
+
+	public void setActivarProfesion(boolean activarProfesion) {
+		if (!activarProfesion) {
+			profesion="";
+		}
+		this.activarProfesion = activarProfesion;
+	}
+
+	public boolean isActivarEspecializacion() {
+		return activarEspecializacion;
+	}
+
+	public void setActivarEspecializacion(boolean activarEspecializacion) {
+		if (!activarEspecializacion) {
+			especializacion="";
+		}
+		this.activarEspecializacion = activarEspecializacion;
+	}
+
+	public boolean isActivarMesesExperiencia() {
+		return activarMesesExperiencia;
+	}
+
+	public void setActivarMesesExperiencia(boolean activarMesesExperiencia) {
+		if (!activarMesesExperiencia) {
+			mesesExperiencia=0;
+		}
+		this.activarMesesExperiencia = activarMesesExperiencia;
+	}
+
 	public ArrayList<HojaVida> getHojasVida() {
 		return hojasVida;
 	}
@@ -78,8 +128,19 @@ public class SeleccionBean {
 	}
 
 	public String seleccionarHojasVida(){
-		hojasVida=gestionModelo.consultarHojasVida(edadMinima, edadMaxima, profesion, especializacion, mesesExperiencia);
-		return "resultadosseleccion.xhtml?faces-redirect=true";
+		if (!activarEdad && !activarProfesion && !activarEspecializacion && !activarMesesExperiencia) {
+			FacesMessage mensajeEmergente=new FacesMessage(FacesMessage.SEVERITY_WARN, "No existe ningún criterio de búsqueda asignado", "");
+			FacesContext.getCurrentInstance().addMessage(null, mensajeEmergente);
+			return "";
+		} else {
+			hojasVida=gestionModelo.consultarHojasVida(edadMinima, edadMaxima, profesion, especializacion, mesesExperiencia);
+			return "resultadosseleccion.xhtml?faces-redirect=true";
+		}
+	}
+	
+	private void establecerRangoEdades(){
+		edadMinima=18;
+		edadMaxima=70;
 	}
 
 }
